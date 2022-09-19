28922f84e1858a8747c1ac332e4a7cffce67f4b8
ekeycept <gorbonosdd@mail.ru>
2022-09-20 00:36:33 +0300

    Change Level Creator
    Level Creator can delete platforms with coroutine. But it still can't add new platforms

--- Stranger in loop/Assets/Script/LevelCreator.cs
+++ Stranger in loop/Assets/Script/LevelCreator.cs
@@ -4,18 +4,32 @@ using UnityEngine;
 
 public class LevelCreator : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject[] platformPrefab;
+    [SerializeField] private GameObject[] platformPrefab;
+    [SerializeField] private GameObject[] platforms;
     private int randomPrefab;
     private int randomX;
+    private int i;
 
     void Start()
     {
-        for (int i = 0; i < 50; i++)
+        for (i = 0; i < 50; i++)
         {
             randomPrefab = Random.Range(0, 3);
             randomX = Random.Range(-5, 5);
-            Instantiate(platformPrefab[randomPrefab], new Vector3(randomX, 0, (i * 11)), Quaternion.Euler(-90, 0, 0));            
+            platforms[i] = Instantiate(platformPrefab[randomPrefab], new Vector3(randomX, 0, (i * 11)), Quaternion.Euler(-90, 0, 0));
+        }
+        StartCoroutine(LoopFunction(3));
+    }
+
+
+    private IEnumerator LoopFunction(float waitTime)
+    {
+        int i = 0;
+        while (i >= 0)
+        {
+            Destroy(platforms[i]);
+            yield return new WaitForSeconds(waitTime);
+            i++;
         }
     }
 }
