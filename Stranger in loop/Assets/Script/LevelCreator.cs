a327e9f22b50e8ae3f9ad177d33c9fa3085fe918
ekeycept <gorbonosdd@mail.ru>
2022-09-20 20:14:38 +0300

    Level Creator Update
    New platforms after every 25 score points

--- Stranger in loop/Assets/Script/LevelCreator.cs
+++ Stranger in loop/Assets/Script/LevelCreator.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
 
 public class LevelCreator : MonoBehaviour
 {
     [SerializeField] private GameObject[] platformPrefab;
     [SerializeField] private GameObject[] platforms;
+    [SerializeField] private Text Score;
+    [SerializeField] private Transform Player;
+    [SerializeField] private int lastPos = 0;
+    private int j = 2;
     private int randomPrefab;
     private int randomX;
     private int i;
@@ -14,13 +20,31 @@ public class LevelCreator : MonoBehaviour
     {
         for (i = 0; i < 50; i++)
         {
-            randomPrefab = Random.Range(0, 3);
-            randomX = Random.Range(-5, 5);
+            randomPrefab = UnityEngine.Random.Range(0, 3);
+            randomX = UnityEngine.Random.Range(-5, 5);
             platforms[i] = Instantiate(platformPrefab[randomPrefab], new Vector3(randomX, 0, (i * 11)), Quaternion.Euler(-90, 0, 0));
         }
         StartCoroutine(LoopFunction(3));
     }
 
+    private void Update()
+    {
+        int currentPos = Convert.ToInt32(Player.position.z / 4.6f);
+        int distance = currentPos - lastPos;
+
+        if (distance >= 25)
+        {
+            for (i = 0; i < 25; i++)
+            {
+                randomPrefab = UnityEngine.Random.Range(0, 3);
+                randomX = UnityEngine.Random.Range(-5, 5);
+                platforms[j*25 + i] = Instantiate(platformPrefab[randomPrefab], new Vector3(randomX, 0, platforms[j*25 - 1 + i].transform.position.z + 11), Quaternion.Euler(-90, 0, 0));
+            }
+            Debug.Log("New platforms created");
+            j++;
+            lastPos = currentPos;
+        }
+    }
 
     private IEnumerator LoopFunction(float waitTime)
     {
