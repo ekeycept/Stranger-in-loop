ca793b265bd7bc5b23050a9158cce6e0f4a410af
ekeycept <gorbonosdd@mail.ru>
2022-09-22 14:41:35 +0300

    Level Creator - Code Refactoring
    Created a method AddPlatforms()

--- Stranger in loop/Assets/Script/LevelCreator.cs
+++ Stranger in loop/Assets/Script/LevelCreator.cs
@@ -10,7 +10,7 @@ public class LevelCreator : MonoBehaviour
     [SerializeField] private GameObject[] platformPrefabSecond;
     [SerializeField] private GameObject[] platformPrefabThird;
     [SerializeField] private GameObject[] platformPrefabFourth;
-    [SerializeField] private GameObject[] platformPrefabFive;
+    [SerializeField] private GameObject[] platformPrefabFifth;
     [SerializeField] private GameObject[] platforms;
     [SerializeField] private Text Score;
     [SerializeField] private Transform Player;
@@ -34,89 +34,45 @@ public class LevelCreator : MonoBehaviour
     private void Update()
     {
         int currentPos = Convert.ToInt32(Player.position.z / 4.6f);
-        int distance = currentPos - lastPos;
 
         if (currentPos < 100)
         {
-            if (distance >= 25)
-            {
-                Array.Resize(ref platforms, platforms.Length + 25); 
-                for (i = 0; i < 25; i++)
-                {
-                    randomPrefab = UnityEngine.Random.Range(0, 3);
-                    randomX = UnityEngine.Random.Range(-5, 5);
-                    platforms[j*25 + i] = Instantiate(platformPrefab[randomPrefab], new Vector3(randomX, 0, platforms[j*25 - 1 + i].transform.position.z + 11), Quaternion.Euler(-90, 0, 0));
-                }
-                Debug.Log("New platforms created");
-                j++;
-                lastPos = currentPos;
-            }
+            AddPlatforms(platformPrefab, currentPos, lastPos);
         }
         else if (currentPos > 100 && currentPos < 200)
         {
-            if (distance >= 25)
-            {
-                Array.Resize(ref platforms, platforms.Length + 25);
-                for (i = 0; i < 25; i++)
-                {
-                    randomPrefab = UnityEngine.Random.Range(0, 3);
-                    randomX = UnityEngine.Random.Range(-5, 5);
-                    platforms[j * 25 + i] = Instantiate(platformPrefabSecond[randomPrefab], new Vector3(randomX, 0, platforms[j * 25 - 1 + i].transform.position.z + 11), Quaternion.Euler(-90, 0, 0));
-                }
-                Debug.Log("New platforms created");
-                j++;
-                lastPos = currentPos;
-            }
+            AddPlatforms(platformPrefabSecond, currentPos, lastPos);
         }
         else if (currentPos > 200 && currentPos < 300)
         {
-            if (distance >= 25)
-            {
-                Array.Resize(ref platforms, platforms.Length + 25);
-                for (i = 0; i < 25; i++)
-                {
-                    randomPrefab = UnityEngine.Random.Range(0, 3);
-                    randomX = UnityEngine.Random.Range(-5, 5);
-                    platforms[j * 25 + i] = Instantiate(platformPrefabThird[randomPrefab], new Vector3(randomX, 0, platforms[j * 25 - 1 + i].transform.position.z + 11), Quaternion.Euler(-90, 0, 0));
-                }
-                Debug.Log("New platforms created");
-                j++;
-                lastPos = currentPos;
-            }
+            AddPlatforms(platformPrefabThird, currentPos, lastPos);
         }
         else if (currentPos > 300 && currentPos < 400)
         {
-            if (distance >= 25)
-            {
-                Array.Resize(ref platforms, platforms.Length + 25);
-                for (i = 0; i < 25; i++)
-                {
-                    randomPrefab = UnityEngine.Random.Range(0, 3);
-                    randomX = UnityEngine.Random.Range(-5, 5);
-                    platforms[j * 25 + i] = Instantiate(platformPrefabFourth[randomPrefab], new Vector3(randomX, 0, platforms[j * 25 - 1 + i].transform.position.z + 11), Quaternion.Euler(-90, 0, 0));
-                }
-                Debug.Log("New platforms created");
-                j++;
-                lastPos = currentPos;
-            }
+            AddPlatforms(platformPrefabFourth, currentPos, lastPos);
         }
         else if (currentPos > 400)
         {
-            if (distance >= 25)
-            {
-                Array.Resize(ref platforms, platforms.Length + 25);
-                for (i = 0; i < 25; i++)
-                {
-                    randomPrefab = UnityEngine.Random.Range(0, 3);
-                    randomX = UnityEngine.Random.Range(-5, 5);
-                    platforms[j * 25 + i] = Instantiate(platformPrefabFive[randomPrefab], new Vector3(randomX, 0, platforms[j * 25 - 1 + i].transform.position.z + 11), Quaternion.Euler(-90, 0, 0));
-                }
-                Debug.Log("New platforms created");
-                j++;
-                lastPos = currentPos;
-            }
+            AddPlatforms(platformPrefabFifth, currentPos, lastPos);
         }
 
+    }
+
+    private void AddPlatforms(GameObject[] prefabs, float currentPos, float lastPos)
+    {
+        if ((currentPos - lastPos) >= 25)
+        {
+            Array.Resize(ref platforms, platforms.Length + 25);
+            for (i = 0; i < 25; i++)
+            {
+                randomPrefab = UnityEngine.Random.Range(0, 3);
+                randomX = UnityEngine.Random.Range(-5, 5);
+                platforms[j * 25 + i] = Instantiate(platformPrefabThird[randomPrefab], new Vector3(randomX, 0, platforms[j * 25 - 1 + i].transform.position.z + 11), Quaternion.Euler(-90, 0, 0));
+            }
+            Debug.Log("New platforms created");
+            j++;
+            lastPos = currentPos;
+        }
     }
 
     private IEnumerator LoopFunction(float waitTime)
