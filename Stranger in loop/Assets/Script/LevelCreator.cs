8107fcab5c03b122b09b3b8d325dad47a30cc5af
ekeycept <gorbonosdd@mail.ru>
2022-09-23 15:01:30 +0300

    Level Manager - Decorating System
    Add decorating system with random generation

--- Stranger in loop/Assets/Script/LevelCreator.cs
+++ Stranger in loop/Assets/Script/LevelCreator.cs
@@ -13,14 +13,22 @@ public class LevelCreator : MonoBehaviour
     [SerializeField] private GameObject[] platformPrefabFifth;
     [SerializeField] private GameObject[] decoratingPlatformPrefab;
     [SerializeField] private GameObject[] platforms;
+    [SerializeField] private GameObject[] decoratingPlatforms;
     [SerializeField] private Text Score;
     [SerializeField] private Transform Player;
     [SerializeField] private int lastPos = 0;
     private int j = 1;
     private int randomPrefab;
     private int randomX;
+    private int randomY;
+    private int randomRotX;
+    private int randomRotY;
+    private int randomRotZ;
     private int i;
     private int randomMovingTile;
+    private int randomSide;
+    private int k;
+    int startPos = 0;
 
     void Start()
     {
@@ -30,6 +38,30 @@ public class LevelCreator : MonoBehaviour
             randomX = UnityEngine.Random.Range(-5, 5);
             platforms[i] = Instantiate(platformPrefab[randomPrefab], new Vector3(randomX, 0, (i * 11)), Quaternion.Euler(-90, 0, 0));
         }
+        for (int i = 0; i < 100; i++)
+        {
+            randomSide = UnityEngine.Random.Range(0, 2);
+            if (randomSide == 0)
+            {
+                randomPrefab = UnityEngine.Random.Range(0, 4);
+                randomX = UnityEngine.Random.Range(-35, -15);
+                randomY = UnityEngine.Random.Range(-20, 20);
+                randomRotX = UnityEngine.Random.Range(-90, 90);
+                randomRotY = UnityEngine.Random.Range(-90, 90);
+                randomRotZ = UnityEngine.Random.Range(-90, 90);
+                decoratingPlatforms[i] = Instantiate(decoratingPlatformPrefab[randomPrefab], new Vector3(randomX, randomY, i * 10), Quaternion.Euler(randomRotX, randomRotY, randomRotZ));
+            }
+            if (randomSide == 1)
+            {
+                randomPrefab = UnityEngine.Random.Range(0, 4);
+                randomX = UnityEngine.Random.Range(15, 35);
+                randomY = UnityEngine.Random.Range(-20, 20); 
+                randomRotX = UnityEngine.Random.Range(-90, 90);
+                randomRotY = UnityEngine.Random.Range(-90, 90);
+                randomRotZ = UnityEngine.Random.Range(-90, 90);
+                decoratingPlatforms[i] = Instantiate(decoratingPlatformPrefab[randomPrefab], new Vector3(randomX, randomY, i * 10), Quaternion.Euler(randomRotX, randomRotY, randomRotZ));
+            }
+        }
         StartCoroutine(LoopFunction(3));
     }
 
@@ -37,26 +69,20 @@ public class LevelCreator : MonoBehaviour
     {
         int currentPos = Convert.ToInt32(Player.position.z / 4.6f);
 
-        if (currentPos < 100)
+        if (currentPos <= 100)
         {
-            AddPlatforms(platformPrefab, currentPos, ref lastPos);     
+            AddDecoratingPlatforms(decoratingPlatformPrefab, currentPos, ref startPos, 1);
+            AddPlatforms(platformPrefab, currentPos, ref lastPos);
         }
         else if (currentPos > 100 && currentPos < 200)
-        {
             AddPlatforms(platformPrefabSecond, currentPos, ref lastPos);
-        }
         else if (currentPos > 200 && currentPos < 300)
-        {
             AddPlatforms(platformPrefabThird, currentPos, ref lastPos);
-        }
         else if (currentPos > 300 && currentPos < 400)
-        {
             AddPlatforms(platformPrefabFourth, currentPos, ref lastPos);
-        }
         else if (currentPos > 400)
-        {
             AddPlatforms(platformPrefabFifth, currentPos, ref lastPos);
-        }
+
 
     }
 
@@ -87,6 +113,34 @@ public class LevelCreator : MonoBehaviour
         }
     }
 
+    private void AddDecoratingPlatforms(GameObject[] prefab, int currentPos, ref int startPos, int k)
+    {
+        if (currentPos - startPos >= 99)
+        {
+            Array.Resize(ref decoratingPlatforms, decoratingPlatforms.Length + 50);
+            for (int i = 0; i < 100; i++)
+            {
+                randomSide = UnityEngine.Random.Range(0, 2);
+                if (randomSide == 0)
+                {
+                    randomPrefab = UnityEngine.Random.Range(0, 4);
+                    randomX = UnityEngine.Random.Range(-25, -15);
+                    randomY = UnityEngine.Random.Range(-10, 10);
+                    decoratingPlatforms[100 * k + i] = Instantiate(prefab[randomPrefab], new Vector3(randomX, randomY, decoratingPlatforms[k * 100 - 1 + i].transform.position.z + 30), Quaternion.Euler(-90, 0, 0));
+                }
+                if (randomSide == 1)
+                {
+                    randomPrefab = UnityEngine.Random.Range(0, 4);
+                    randomX = UnityEngine.Random.Range(15, 25);
+                    randomY = UnityEngine.Random.Range(-10, 10);
+                    decoratingPlatforms[100 * k + i] = Instantiate(prefab[randomPrefab], new Vector3(randomX, randomY, decoratingPlatforms[k * 100 - 1 + i].transform.position.z + 30), Quaternion.Euler(-90, 0, 0));
+                }
+            }
+            Debug.Log("Created decorating platforms!");
+            startPos += 100;
+
+        }
+    }
 
     private IEnumerator LoopFunction(float waitTime)
     {
